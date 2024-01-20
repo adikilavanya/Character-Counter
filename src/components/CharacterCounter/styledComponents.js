371bc5121b31081c9a902e14a39d41a903427e2e
lavanya <adikilavanya.2001@gmail.com>
2024-01-20 11:23:25 +0000

    sloution

---
+++ src/components/CharacterCounter/styledComponents.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const LeftPanel = styled.p`
+  color: black;
+`
+export const InfoCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const Info = styled.p`
+  color: black;
+`
+export const UserInputsList = styled.p`
+  color: black;
+`
+export const RightPanel = styled.h1`
+  color: black;
+`
+export const CounterHeading = styled.p`
+  color: black;
+`
+export const AddInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const Input = styled.input`
+  color: black;
+`
+export const AddInputButton = styled.btn`
+  color: black;
+`
+export const EmptyImage = styled.img`
+  background-size: cover;
+`
